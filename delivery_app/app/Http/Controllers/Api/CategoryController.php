2ec30b65ee01f279ec3a20464a8b068c249d6225
ariel cayo <ariel.cayo.vargas@gmail.com>
2025-01-08 14:42:14 -0400

    modif

--- delivery_app/app/Http/Controllers/Api/CategoryController.php
+++ delivery_app/app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -13,6 +14,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::all();
+        return response()->json($categories);
     }
 
     /**
