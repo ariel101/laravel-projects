832a7c573d1fe43fc7508bd58245ea512eeb2e6c
ariel101 <ariel.cayo.vargas@gmail.com>
2025-03-06 18:32:51 -0400

    Revert "README"
    This reverts commit 2f8de22f663369a98540b48e3885a856255af4b0.

--- delivery_app/app/Http/Controllers/Api/OrderController.php
+++ delivery_app/app/Http/Controllers/Api/OrderController.php
@@ -3,23 +3,107 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cart;
 use App\Models\Order;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-   public function createOrder(){
-    $client = Auth::user();
-    $cart = Cart::where('client_id', $client->id)->get();
-
-    if($cart->empty()){
-        return response()->json(['message'=> 'el carrito esta vacio'], 404);
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $order = Order::all();
+        return response()->json(
+            [
+                'status' => 'success',
+                'data' => $order
+            ]
+            );
     }
 
-    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $input = $request->all();
+        $order = Order::create($input);
+        return response()->json(
+            [
+                'status' => 'success',
+                'data' => $order
+            ]
+            );
+    }
 
-   }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+        $order = Order::find($id);
+        if($order){
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'data' => $order
+                ]
+                );
+        }else{
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'No se encontró la orden'
+                ]
+                );
+        }
+    }
 
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+        $input = $request->all();
+        $order = Order::find($id);
+        if($order){
+            $order->update($input);
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'data' => $order
+                ]
+                );
+        }else{
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'No se encontró la orden'
+                ]
+                );
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        $order = Order::find($id);
+        if(! $order){
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'No se encontró la orden'
+                ]
+                );
+        }
+        $order->delete();
+    }
 }
